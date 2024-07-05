27b1c369a7404493be43195576de3c18384781d8
Sujithra-dhayalan <sujithradhayalan@gmail.com>
2024-07-05 18:49:00 +0530

    js

---
+++ day-8/basics.js
@@ -0,0 +1,7 @@
+var a=2200;
+var b="Web development"
+console.log("The fee of this",b,"is",a);
+
+var l=90
+var b=80
+console.log(l*b)
---
+++ day-8/fun.js
@@ -0,0 +1,10 @@
+function OddorEven(l){
+    if (l%2===0){
+        console.log("Even")
+    }
+    else{
+        console.log("Odd")
+    }
+}
+var num=90
+OddorEven(num)
